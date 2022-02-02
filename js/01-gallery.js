aa91f0fe7304bacfff46e1c77afe8bcc93001d68
Alexander Bohatskyi <obogatskiy@icloud.com>
2022-02-02 22:45:54 +0200

    Final 7hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerImg = document.querySelector('.gallery')
+
+const makeImageMarkup = ({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+}
+
+const makeGalerryMarkup = galleryItems.map(makeImageMarkup).join('')
+containerImg.insertAdjacentHTML('afterbegin', makeGalerryMarkup)
+
+
+containerImg.addEventListener('click', getUrl)
+
+function getUrl(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return; 
+    }
+    event.preventDefault()
+    const selectedUrl = event.target.dataset.source;
+
+
+    const instance = basicLightbox.create(`
+        <img src="${selectedUrl}" width="800" height="600">
+    `)
+
+    instance.show()
+}
+
+
 
 console.log(galleryItems);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerImg = document.querySelector('.gallery')
+
+const makeImageMarkup = ({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}
+
+
+const makeGalerryMarkup = galleryItems.map(makeImageMarkup).join('')
+containerImg.insertAdjacentHTML('afterbegin', makeGalerryMarkup)
+
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt", captionPosition: "bottom", captionDelay: 250
+});
 
 console.log(galleryItems);
